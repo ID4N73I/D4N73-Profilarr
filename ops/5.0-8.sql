8b49effef18db5aabe4306661398fe3720a89185
ID4N73I <cyvi@live.de>
2026-06-06 22:51:07 +0200

    0.8

---
+++ ops/5.0-8.sql
@@ -0,0 +1,160 @@
+-- @operation: export
+-- @entity: batch
+-- @name: 0.8
+-- @exportedAt: 2026-06-06T20:51:05.891Z
+-- @opIds: 1161, 1162, 1163, 1164, 1165, 1166, 1167, 1168, 1169, 1170, 1171, 1172, 1173, 1174, 1175, 1176, 1177, 1178, 1179, 1180, 1181, 1182, 1183, 1184, 1185, 1186, 1187, 1188, 1189, 1190, 1191, 1192, 1193, 1194, 1195, 1196, 1197
+
+-- --- BEGIN op 1161 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'Bluray-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1161
+
+-- --- BEGIN op 1162 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'HDTV-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1162
+
+-- --- BEGIN op 1163 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'Remux-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1163
+
+-- --- BEGIN op 1164 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'WEBDL-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1164
+
+-- --- BEGIN op 1165 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'WEBRip-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1165
+
+-- --- BEGIN op 1166 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'BR-DISK' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1166
+
+-- --- BEGIN op 1167 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'Bluray-480p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1167
+
+-- --- BEGIN op 1168 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'Bluray-576p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1168
+
+-- --- BEGIN op 1169 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'CAM' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1169
+
+-- --- BEGIN op 1170 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'DVD' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1170
+
+-- --- BEGIN op 1171 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'DVD-R' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1171
+
+-- --- BEGIN op 1172 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'DVDSCR' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1172
+
+-- --- BEGIN op 1173 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'REGIONAL' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1173
+
+-- --- BEGIN op 1174 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'Raw-HD' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1174
+
+-- --- BEGIN op 1175 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'SDTV' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1175
+
+-- --- BEGIN op 1176 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'TELECINE' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1176
+
+-- --- BEGIN op 1177 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'TELESYNC' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1177
+
+-- --- BEGIN op 1178 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'Unknown' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1178
+
+-- --- BEGIN op 1179 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'WEBDL-480p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1179
+
+-- --- BEGIN op 1180 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'WEBRip-480p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1180
+
+-- --- BEGIN op 1181 ( update radarr_quality_definitions "Radarr" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Radarr' and "quality_name" = 'WORKPRINT' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1181
+
+-- --- BEGIN op 1182 ( update sonarr_quality_definitions "Sonarr" )
+update "sonarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Sonarr' and "quality_name" = 'Bluray-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1182
+
+-- --- BEGIN op 1183 ( update sonarr_quality_definitions "Sonarr" )
+update "sonarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Sonarr' and "quality_name" = 'Bluray-480p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1183
+
+-- --- BEGIN op 1184 ( update sonarr_quality_definitions "Sonarr" )
+update "sonarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Sonarr' and "quality_name" = 'Bluray-576p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1184
+
+-- --- BEGIN op 1185 ( update sonarr_quality_definitions "Sonarr" )
+update "sonarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Sonarr' and "quality_name" = 'DVD' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1185
+
+-- --- BEGIN op 1186 ( update sonarr_quality_definitions "Sonarr" )
+update "sonarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Sonarr' and "quality_name" = 'HDTV-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1186
+
+-- --- BEGIN op 1187 ( update sonarr_quality_definitions "Sonarr" )
+update "sonarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Sonarr' and "quality_name" = 'Raw-HD' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1187
+
+-- --- BEGIN op 1188 ( update sonarr_quality_definitions "Sonarr" )
+update "sonarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Sonarr' and "quality_name" = 'Remux-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1188
+
+-- --- BEGIN op 1189 ( update sonarr_quality_definitions "Sonarr" )
+update "sonarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Sonarr' and "quality_name" = 'SDTV' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1189
+
+-- --- BEGIN op 1190 ( update sonarr_quality_definitions "Sonarr" )
+update "sonarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Sonarr' and "quality_name" = 'Unknown' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1190
+
+-- --- BEGIN op 1191 ( update sonarr_quality_definitions "Sonarr" )
+update "sonarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Sonarr' and "quality_name" = 'WEBDL-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1191
+
+-- --- BEGIN op 1192 ( update sonarr_quality_definitions "Sonarr" )
+update "sonarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Sonarr' and "quality_name" = 'WEBDL-480p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1192
+
+-- --- BEGIN op 1193 ( update sonarr_quality_definitions "Sonarr" )
+update "sonarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Sonarr' and "quality_name" = 'WEBRip-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1193
+
+-- --- BEGIN op 1194 ( update sonarr_quality_definitions "Sonarr" )
+update "sonarr_quality_definitions" set "min_size" = 0, "max_size" = 2, "preferred_size" = 1 where "name" = 'Sonarr' and "quality_name" = 'WEBRip-480p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 1194
+
+-- --- BEGIN op 1195 ( update quality_profile "Anime-Series 720p/1080p" )
+INSERT INTO quality_profile_custom_formats (quality_profile_name, custom_format_name, arr_type, score)
+SELECT 'Anime-Series 720p/1080p', 'Codec - h266', 'sonarr', 50
+WHERE NOT EXISTS (
+  SELECT 1 FROM quality_profile_custom_formats
+  WHERE quality_profile_name = 'Anime-Series 720p/1080p'
+    AND custom_format_name = 'Codec - h266'
+    AND arr_type = 'sonarr'
+);
+-- --- END op 1195
+
+-- --- BEGIN op 1196 ( update radarr_media_settings "Radarr" )
+update "radarr_media_settings" set "enable_media_info" = 0 where "name" = 'Radarr' and "enable_media_info" = 1;
+-- --- END op 1196
+
+-- --- BEGIN op 1197 ( update sonarr_media_settings "Sonarr" )
+update "sonarr_media_settings" set "enable_media_info" = 0 where "name" = 'Sonarr' and "enable_media_info" = 1;
+-- --- END op 1197
